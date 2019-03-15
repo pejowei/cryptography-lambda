b0cdf33a5efae628d5758b4f871977c874a70c2e
EC2 Default User <ec2-user@ip-172-31-36-147.us-east-2.compute.internal>
2019-03-15 14:10:30 +0000

    initial commit

---
+++ build.sh
@@ -0,0 +1,21 @@
+CRYPTOGRAPHY_VERSION=2.6.1
+DEST_PY36=python-3.6/cryptography
+
+indent() { sed 's/^/    /'; }
+
+
+rm -rf $DEST_PY36
+mkdir -p $DEST_PY36
+
+docker build -t psycopg2-py27-py36 \
+             --build-arg cryptography_version=$CRYPTOGRAPHY_VERSION \
+             --no-cache \
+             -f Dockerfile-py36 . | indent
+
+# Get py36 versions
+id=$(docker create cryptography-py36)
+
+docker cp $id:/app/cryptography-py36/build/lib.linux-x86_64-3.6/cryptography/. $DEST_PY36 | indent
+docker cp $id:/app/cryptography-py36/lib/. $DEST_PY36 | indent
+
+docker rm -v $id | indent
